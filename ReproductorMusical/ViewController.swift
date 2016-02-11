d13b4513dd825714b04b7172db0a94386f1ac206
Jose Luis Garcia Dueñas <joseluisgarciad@gmail.com>
2016-02-11 16:04:44 +0100

    Initial Commit

---
+++ ReproductorMusical/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReproductorMusical
+//
+//  Created by Jose Luis Garcia Dueñas on 11/2/16.
+//  Copyright © 2016 Jose Luis Garcia Dueñas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
